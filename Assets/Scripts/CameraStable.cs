8a1f2b82e70114ca8d5d764fe76badf68e0403ec
Manasbi Parajuli <mparajul@ramapo.edu>
2019-04-21 01:06:09 -0400

    Implemented Player Input and Added Trees to Terrain

---
+++ Assets/Scripts/CameraStable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraStable : MonoBehaviour
+{
+	[SerializeField] public GameObject raceCar;
+
+	public float raceCarX;
+	public float raceCarY;
+	public float raceCarZ;
+
+    // Update is called once per frame
+    void Update()
+    {
+		raceCarX = raceCar.transform.eulerAngles.x;
+		raceCarY = raceCar.transform.eulerAngles.y;
+		raceCarZ = raceCar.transform.eulerAngles.z;
+
+		// keep camera axis fixed on y-axis so that camera follows the car when it tilts
+		transform.eulerAngles = new Vector3(raceCarX - raceCarX, raceCarY, raceCarZ - raceCarZ);
+    }
+}
