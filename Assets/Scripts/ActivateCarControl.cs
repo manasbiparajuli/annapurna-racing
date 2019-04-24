900964835071bcca04fadd8d21a8f2d0717e1357
Manasbi Parajuli <mparajul@ramapo.edu>
2019-04-24 00:41:51 -0400

    Implemented Race Countdown Using Animations

---
+++ Assets/Scripts/ActivateCarControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateCarControl : MonoBehaviour
+{
+	public GameObject CarControl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		CarControl.GetComponent<CarController>().enabled = true;		        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/RaceCountdown.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RaceCountdown : MonoBehaviour
+{
+	[SerializeField] public GameObject countDown;
+	[SerializeField] public GameObject LapTimer;
+	[SerializeField] public GameObject CarControls;
+
+	[SerializeField] public AudioSource getReady;
+	[SerializeField] public AudioSource goAudio;
+
+	void Start()
+    {
+		StartCoroutine(CountStart());        
+    }
+
+	IEnumerator CountStart()
+	{
+		// Start playing "Get Ready" audio
+		// Countdown is 3
+		yield return new WaitForSeconds(0.5f);
+		countDown.GetComponent<Text>().text = "3";
+		getReady.Play();
+		countDown.SetActive(true);
+
+		// Countdown is 2
+		yield return new WaitForSeconds(1);
+		countDown.SetActive(false);
+		countDown.GetComponent<Text>().text = "2";
+		getReady.Play();
+		countDown.SetActive(true);
+
+		// Countdown is 1
+		yield return new WaitForSeconds(1);
+		countDown.SetActive(false);
+		countDown.GetComponent<Text>().text = "1";
+		getReady.Play();
+		countDown.SetActive(true);
+
+		// Countdown has ended. Player can now start racing
+		// Play "Go Audio" to signal the start of the race
+		yield return new WaitForSeconds(1);
+		countDown.SetActive(false);
+		goAudio.Play();
+
+		// Initiate our lap timer and allow the player to control the car
+		LapTimer.SetActive(true);
+		CarControls.SetActive(true);
+	}
+}
