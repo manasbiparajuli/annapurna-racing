efb308029f8fe5bb53060c7e2d0b1e5354d39af9
Manasbi Parajuli <mparajul@ramapo.edu>
2019-04-24 23:50:07 -0400

    Imported Car in Standard Assets and Setup AI Waypoints

---
+++ Assets/Scripts/AICarTrack.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AICarTrack : MonoBehaviour
+{
+	//public GameObject Tracker;
+	//public GameObject[] AITracker = new GameObject[26];
+	//public int trackerNumber = 0;
+
+	// Update is called once per frame
+	void Update()
+    {
+		//if (trackerNumber == 0)
+		//{
+			//Tracker.transform.position = AITracker[1].transform.position;
+		//}
+    }
+
+
+}
--- Assets/Scripts/ActivateCarControl.cs
+++ Assets/Scripts/ActivateCarControl.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
 
 public class ActivateCarControl : MonoBehaviour
 {
