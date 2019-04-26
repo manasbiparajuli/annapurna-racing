f87ced9683499c17621dad8c05c78ee4e643f913
Manasbi Parajuli <mparajul@ramapo.edu>
2019-04-25 23:37:15 -0400

    Added scenes for main menu, track selection and added minimap

---
+++ Assets/Scripts/ButtonClicks.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonClicks : MonoBehaviour
+{
+	public void PlayGame()
+	{
+		SceneManager.LoadScene(2);
+	}
+
+	public void TrackSelect()
+	{
+		SceneManager.LoadScene(1);
+	}
+
+	public void MainMenu()
+	{
+		SceneManager.LoadScene(0);
+	}
+
+	public void QuitGame()
+	{
+		// save any game data here
+		#if UNITY_EDITOR
+			// Application.Quit() does not work in the editor so
+			// UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+			UnityEditor.EditorApplication.isPlaying = false;
+		#else
+			Application.Quit();
+		#endif
+	}
+
+	// Button clicks for track selections
+	public void TrackAnnapurnaCircuit()
+	{
+		SceneManager.LoadScene(2);
+	}
+}
