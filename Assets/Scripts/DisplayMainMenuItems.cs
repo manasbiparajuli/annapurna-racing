0d64bee2b6da3f2e37df76d3d6f061b584412991
Manasbi Parajuli <mparajul@ramapo.edu>
2019-04-27 23:51:56 -0400

    Added overlay to hide main menu items

---
+++ Assets/Scripts/DisplayMainMenuItems.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayMainMenuItems : MonoBehaviour
+{
+	[SerializeField] public GameObject largeButton;
+	[SerializeField] public GameObject textClick;
+	[SerializeField] public GameObject menuItems;
+
+	public void MainMenuItems()
+	{
+		// Remove the overlay after user clicks on the screen
+		// Then, display the menu items
+		textClick.SetActive(false);
+		menuItems.SetActive(true);
+		largeButton.SetActive(false);
+	}
+}
