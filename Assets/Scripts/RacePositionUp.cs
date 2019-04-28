a18722d67ce9032412063100b9fc26ff87d5652e
Manasbi Parajuli <mparajul@ramapo.edu>
2019-04-28 01:02:00 -0400

    Implemented Race Position for minimum of two cars

---
+++ Assets/Scripts/RacePositionUp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RacePositionUp : MonoBehaviour
+{
+	public GameObject displayPosition;
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.tag.Equals("CarPosition"))
+		{
+			displayPosition.GetComponent<Text>().text = "FIRST";
+		}
+	}
+}
